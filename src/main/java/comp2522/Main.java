44c31d26ef8fa2803078148f1a2ef8528e9395f5
Justin Ng <jng193@my.bcit.ca>
2022-09-07 21:24:42 -0700

    Added new message in Main.java

---
+++ src/main/java/comp2522/Main.java
@@ -0,0 +1,8 @@
+package comp2522;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hi! My favourite hockey team is the Vancouver Canucks!");
+    }
+}
