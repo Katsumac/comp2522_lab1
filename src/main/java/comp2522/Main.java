350808caa71710d1b025e2d151b81793e36dfe1d
Justin Ng <jng193@my.bcit.ca>
2022-09-07 21:44:20 -0700

    Added Javadoc and fixed Google Checks Checkstyle errors

--- src/main/java/comp2522/Main.java
+++ src/main/java/comp2522/Main.java
@@ -1,8 +1,10 @@
 package comp2522;
 
+/**
+ * Prints my favourite hockey team.
+ */
 public class Main {
-    public static void main(String[] args) {
-
-        System.out.println("Hi! My favourite hockey team is the Vancouver Canucks!");
-    }
+  public static void main(String[] args) {
+    System.out.println("Hi! My favourite hockey team is the Vancouver Canucks!");
+  }
 }
